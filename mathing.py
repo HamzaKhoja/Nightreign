02e1729ca938042f0c39910d76f22764a346a2b1
HamzaKhoja <hamza.hanik@gmail.com>
2025-07-22 14:35:12 +0300

    Fixed Church and Rise locations for Shifting Earths

--- mathing.py
+++
@@ -1,5 +0,0 @@
-xs = [510, 200, 660, 750, 350, 190, 775, 530, 190, 440, 550]
-ys = [225, 350, 360, 350, 390, 550, 570, 620, 690, 680, 775]
-
-for i in range(len(xs)):
-    print(f"{xs[i]/960*100:.2f}, {ys[i]/960*100:.2f}")  # Adjusted to match the original format
